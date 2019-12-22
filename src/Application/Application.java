f7356ec576e65d8fdfdd33458b6a4b4b869cd31d
马承仪 <cxm3593@g.rit.edu>
2019-12-21 22:51:17 -0500

    Now the output is with color

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -6,8 +6,9 @@ import javax.swing.*;
 
 public class Application {
     public static void main(String[] args) {
+        //System.out.println("\033[31m RED");
 
-        WorldMap map = new WorldMap(160,90);
+        WorldMap map = new WorldMap(50,50);
         map.drawMap();
         //System.out.println(map.toString());
         map.print(ViewMode.Altitude);
--- src/Model/WorldMap.java
+++ src/Model/WorldMap.java
@@ -1,20 +1,19 @@
 package Model;
 
-import javax.swing.*;
 import java.util.Random;
 
 public class WorldMap {
 
     private MapSpace map[][];
-    private int length;
-    private int wide;
+    private int row;
+    private int column;
     public final int MaxAltitudeStep = 500;
 
 
-    public WorldMap(int length, int wide){
-        this.length = length;
-        this.wide = wide;
-        map = new MapSpace[length][wide];
+    public WorldMap(int row, int column){
+        this.row = row;
+        this.column = column;
+        map = new MapSpace[row][column];
     }
 
     /**
@@ -31,8 +30,8 @@ public class WorldMap {
      */
     public void initialize(){
         System.out.println("Initializing a map with basic MapSpace...");
-        for(int i=0; i < (this.length-1); i++){
-            for(int j=0; j<(this.wide-1);j++){
+        for(int i=0; i < (this.row); i++){
+            for(int j = 0; j<(this.column); j++){
                 map[i][j] = new MapSpace();
             }
         }
@@ -45,7 +44,12 @@ public class WorldMap {
     public void drawTerrain(int mn){
         Random rand = new Random();
         for(int i=0; i<mn; i++){
-            drawTerrainRec(rand.nextInt(this.length),rand.nextInt(this.wide),rand.nextInt(8000));
+            int x = rand.nextInt(this.row);
+            int y = rand.nextInt(this.column);
+            if(map[x][y].getAltitude() != 0){
+                i=i-1;
+            }
+            drawTerrainRec(x,y,rand.nextInt(8000));
         }
     }
 
@@ -57,13 +61,13 @@ public class WorldMap {
      */
     private void drawTerrainRec(int x, int y,int a){
         Random rand = new Random();
-        if(map[x][y].getAltitude() == 0){ // this process only draws places without initial terrain.
+        if(map[x][y]!= null && map[x][y].getAltitude() == 0){ // this process only draws places without initial terrain.
             if(a >= 200){ //no more mountain to draw if a is smaller than 200
                 map[x][y].setAltitude(a);
-                if(x>0)drawTerrainRec(x-1,y,(a - rand.nextInt(MaxAltitudeStep)));
-                if(x<this.length)drawTerrainRec(x+1,y,(a - rand.nextInt(MaxAltitudeStep)));
-                if(y<this.wide)drawTerrainRec(x,y+1,(a - rand.nextInt(MaxAltitudeStep)));
-                if(y>0)drawTerrainRec(x,y-1,(a - rand.nextInt(MaxAltitudeStep)));
+                if(x>0) drawTerrainRec(x-1,y,(a - rand.nextInt(MaxAltitudeStep)));
+                if(x<this.row-1) drawTerrainRec(x+1,y,(a - rand.nextInt(MaxAltitudeStep)));
+                if(y<this.column-1) drawTerrainRec(x,y+1,(a - rand.nextInt(MaxAltitudeStep)));
+                if(y>0) drawTerrainRec(x,y-1,(a - rand.nextInt(MaxAltitudeStep)));
             }
         }
     }
@@ -74,17 +78,26 @@ public class WorldMap {
      * @return
      */
     public void print(ViewMode vm){
-        JTextArea textarea = new JTextArea();
-        textarea.setTabSize(5);
         System.out.println("Printing the map...");
 
         this.printSeperateLine();
-        for(int i=0; i < (this.length-1); i++){
+        for(int i=0; i < (this.row); i++){
             System.out.print("|\t");
-            for(int j=0; j<(this.wide-1);j++){
+            for(int j = 0; j<(this.column); j++){
                 if(vm == ViewMode.Altitude){
                     int a = map[i][j].getAltitude();
+                    if(a==0){
+                        System.out.print("\u001b[48;5;220m");
+                    }
+                    else if(a>0 && a<1000){
+                        System.out.print("\u001b[48;5;226m");  // 48 for background color; 38 for font color
+                    }else if(a>= 1000 && a<= 3000){
+                        System.out.print("\u001b[48;5;228m");
+                    }else if(a>3000){
+                        System.out.print("\u001b[48;5;230m");
+                    }
                     System.out.format("%5d",a);
+                    System.out.print("\u001b[0m");
                 }
             }
             System.out.println("\t|");
@@ -93,9 +106,9 @@ public class WorldMap {
     }
 
     private void printSeperateLine(){
-        int line_length = this.length * 2;
-        for(int i=0; i<line_length ; i++){
-            System.out.print("_");
+        int line_length = this.row ;
+        for(int i=0; i<this.column; i++){
+            System.out.format("%5s","_");
         }
         System.out.println();
     }
